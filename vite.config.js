7f6196e89cb6526e9d200d66a7261c445340b7c6
graham <grahomie@gmail.com>
2026-05-13 15:39:14 -0400

    updating packages and stuff

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        open: true,
+    },
+    build: {
+        outDir: 'build', // CRA's default build output
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js',
+    }
+})
